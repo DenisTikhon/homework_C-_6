ecefd4b9c223721f5d5176599d5813a9ebe477ea
DenisT <denis@tikhonov.pp.ru>
2022-08-30 13:09:14 +0300

    add task 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задача 41
+Console.Write("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int[] Nums = new int[M];
+while (i < M) {
+    Console.Write("Введите число "+ (i+1) +" :");
+    Nums[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+int count = 0;
+int index = 0;
+while (index < Nums.Length) {
+if (Nums[index] > 0) {count++;}
+index++;
+}
+Console.WriteLine(count);
+//задача 43
